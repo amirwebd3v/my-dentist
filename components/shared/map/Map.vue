aa6a6228724a39bf05366deed90148d661c59019
AmirHossein <amirhidevs@gmail.com>
2024-07-05 11:58:10 +0330

    update Map with new src

--- components/shared/map/Map.vue
+++ components/shared/map/Map.vue
@@ -1,22 +1,24 @@
 <template>
+
   <div class="map">
 
-      <!-- -----------------------------------------------
-            Start Map
-        ----------------------------------------------- -->
+    <!-- -----------------------------------------------
+          Start Map
+      ----------------------------------------------- -->
 
+    <v-container>
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3236.4952624612806!2d51.383067999999994!3d35.787773!
+        2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzXCsDQ3JzE2LjAiTiA1McKwMjInNTkuMCJF!5e0!3m2!1sen!2s!4v172009
+        5973573!5m2!1sen!2s"
+        allowfullscreen="" referrerpolicy="no-referrer-when-downgrade">
+    </iframe>
 
-          <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51986.783358653425!2d51.08234885!3d35.
-          4752395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x
-          3f920be5c0dbc747%3A0x20ad6dbae6a2c2aa!2sRobat%20Karim%2C%20Tehran
-          %20Province%2C%20Iran!5e0!3m2!1sen!2sde!4v1689526779249!5m2!1sen!2sde"
-                  allowfullscreen="" referrerpolicy="no-referrer-when-downgrade">
-          </iframe>
-
-
-      <!-- -----------------------------------------------
-            End Map
-        ----------------------------------------------- -->
-
+    <!-- -----------------------------------------------
+          End Map
+      ----------------------------------------------- -->
+      </v-container>
   </div>
 </template>
+<script setup>
+</script>
