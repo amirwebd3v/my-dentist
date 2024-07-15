ac01271a87f0f16385bf002c36664deaa73e6e58
AmirHossein <amirhidevs@gmail.com>
2024-07-16 02:54:47 +0330

    refactor testimonial design

--- components/section/testimonial/Testimonial.vue
+++ components/section/testimonial/Testimonial.vue
@@ -5,7 +5,7 @@ import type {PropType} from "@vue/runtime-core";
 import type {TestimonialSettings} from "~/utils/types";
 import {storeToRefs} from "pinia";
 import {useTestimonialStore} from "~/store/testimonial";
-import { DateTime } from 'luxon'
+import {DateTime} from 'luxon'
 
 const props = defineProps({
   testimonialSettings: {
@@ -20,82 +20,170 @@ const props = defineProps({
 
 const {testimonials} = storeToRefs(useTestimonialStore())
 
-onBeforeMount(async ()=>{
+onBeforeMount(async () => {
   await useTestimonialStore().fetch()
 })
 
 </script>
 <template>
-  <div>
-    <div class="mini-spacer">
-      <v-container v-if="!!props.testimonialSettings">
-        <!-- -----------------------------------------------
-            Start Testimonial Text
-        ----------------------------------------------- -->
-        <v-row justify="center" class="mt-16">
-          <v-col cols="12" sm="10" md="9" lg="7">
-            <div class="text-center">
-              <h2 class="section-title font-weight-medium">
-                نظرات مراجعه کنندگان
-              </h2>
+  <div class="mini-spacer">
+    <v-container v-if="!!props.testimonialSettings">
+      <!-- Testimonial Text -->
+      <v-row justify="center" class="mt-16">
+        <v-col cols="12" sm="10" md="9" lg="7">
+          <div class="text-center">
+            <h2 class="section-title font-weight-medium">
+              نظرات مراجعه کنندگان
+            </h2>
+          </div>
+        </v-col>
+      </v-row>
+
+      <!-- Testimonial Carousel -->
+      <v-sheet class="scroll-container" elevation="0"
+               :color="props.testimonialSettings.backColor"
+               :rounded="props.testimonialSettings.backRounded"
+               :style="{ '--back-color': props.testimonialSettings.backColor }">
+      <v-row class="my-12 " justify="center" style="width: max-content;">
+        <v-col  data-first v-for="card in testimonials.values()" :key="card.id">
+        <v-card
+            class="mx-auto" :style="`
+                            width: 330px;
+                            height: 250px;
+                            border-color: ${props.testimonialSettings.cardBorderColor} !important;
+                            background-color: ${props.testimonialSettings.cardColor};
+                        `">
+          <v-card-text class="pa-sm-10 pa-5 mx-5" :style="`color: ${props.testimonialSettings.cardContextColor}`">
+            <div class="d-flex align-center mb-5">
+              <v-avatar size="60">
+                <img :src="`${useAppConfig().api.baseUrl +'/storage/'+card.image}`" :alt="card.image" height="60"/>
+              </v-avatar>
+              <div class="mr-3">
+                <h6 class="text-uppercase font-weight-bold font-14">{{ card.full_name }}</h6>
+                <p class="mt-1 w-auto">
+                  {{ card.service }}
+                  <br v-if="card.date !== null">
+                  {{ card.date !== null ? DateTime.fromISO(card.date).toLocaleString(DateTime.DATE_FULL, { locale: 'fa' }) : '' }}
+                </p>
+              </div>
             </div>
-          </v-col>
-        </v-row>
+            <p class="text-body-1 text-black text-justify">"{{ card.content }}"</p>
+          </v-card-text>
+        </v-card>
+        </v-col>
+        <v-col  v-for="card in testimonials.values()" :key="card.id">
+        <v-card
+            class="mx-auto" :style="`
+                            width: 330px;
+                            height: 250px;
+                            border-color: ${props.testimonialSettings.cardBorderColor} !important;
+                            background-color: ${props.testimonialSettings.cardColor};
+                        `">
+          <v-card-text class="pa-sm-10 pa-5 mx-5" :style="`color: ${props.testimonialSettings.cardContextColor}`">
+            <div class="d-flex align-center mb-5">
+              <v-avatar size="60">
+                <img :src="`${useAppConfig().api.baseUrl +'/storage/'+card.image}`" :alt="card.image" height="60"/>
+              </v-avatar>
+              <div class="mr-3">
+                <h6 class="text-uppercase font-weight-bold font-14">{{ card.full_name }}</h6>
+                <p class="mt-1">
+                  {{ card.service }}
+                  <br v-if="card.date !== null">
+                  {{ card.date !== null ? DateTime.fromISO(card.date).toLocaleString(DateTime.DATE_FULL, { locale: 'fa' }) : '' }}
+                </p>
+              </div>
+            </div>
+            <p class="text-body-1 text-black text-justify">"{{ card.content }}"</p>
+          </v-card-text>
+        </v-card>
+        </v-col>
+        <v-col  data-last  v-for="card in testimonials.values()" :key="card.id">
+        <v-card
+            class="mx-auto" :style="`
+                            width: 330px;
+                            height: 250px;
+                            border-color: ${props.testimonialSettings.cardBorderColor} !important;
+                            background-color: ${props.testimonialSettings.cardColor};
+                        `">
+          <v-card-text class="pa-sm-10 pa-5 mx-5" :style="`color: ${props.testimonialSettings.cardContextColor}`">
+            <div class="d-flex align-center mb-5">
+              <v-avatar size="60">
+                <img :src="`${useAppConfig().api.baseUrl +'/storage/'+card.image}`" :alt="card.image" height="60"/>
+              </v-avatar>
+              <div class="mr-3">
+                <h6 class="text-uppercase font-weight-bold font-14">{{ card.full_name }}</h6>
+                <p class="mt-1">
+                  {{ card.service }}
+                  <br v-if="card.date !== null">
+                  {{ card.date !== null ? DateTime.fromISO(card.date).toLocaleString(DateTime.DATE_FULL, { locale: 'fa' }) : '' }}
+                </p>
+              </div>
+            </div>
+            <p class="text-body-1 text-black text-justify">"{{ card.content }}"</p>
+          </v-card-text>
+        </v-card>
+        </v-col>
+        <div class="fade"></div>
+      </v-row>
+            </v-sheet>
 
-        <!-- -----------------------------------------------
-            End Testimonial Text
-        ----------------------------------------------- -->
-        <!-- -----------------------------------------------
-            Start Testimonial
-        ----------------------------------------------- -->
 
-        <v-sheet
-            class="mx-auto"
-            elevation="0"
-            max-width="auto"
-            :color="props.testimonialSettings.backColor"
-            :rounded="props.testimonialSettings.backRounded"
-        >
-          <v-slide-group
-              class="py-4 "
-          >
-            <v-slide-group-item
-                v-for="card in testimonials.values()"
-                :key="card.full_name"
-            >
-              <v-row class="my-12" justify="center">
-                <v-card class="mx-12" max-width="330"
-                        :elevation="props.testimonialSettings.cardElevation"
-                        :variant="props.testimonialSettings.cardVariant"
-                        :style="`height: 250px;
-                         border-color: ${props.testimonialSettings.cardBorderColor} !important;
-                         background-color: ${props.testimonialSettings.cardColor};`"
-                        :border="props.testimonialSettings.cardBorderSize"
-                >
-                  <v-card-text class="pa-sm-10 pa-5 mx-5" :style="`color: ${props.testimonialSettings.cardContextColor}`">
-                    <div class="d-flex align-center mb-5">
-                      <v-avatar size="60">
-                        <img :src="card.image" :alt="card.image" height="60"/>
-                      </v-avatar>
-                      <div class="mr-3">
-                        <h6 class="text-uppercase font-weight-bold font-14">{{ card.full_name }}</h6>
-                        <p class="mt-1"> {{ card.date !== null ? card.service.concat(' | ', DateTime.fromISO(card.date)
-                            .setZone('Asia/Tehran')
-                            .toLocaleString(DateTime.DATE_FULL, { locale: 'fa' })) : card.service }}</p>
-                      </div>
-                    </div>
-                    <p class="text-body-1 text-black text-justify">“{{ card.content }}”</p>
-                  </v-card-text>
-                </v-card>
-              </v-row>
-            </v-slide-group-item>
-          </v-slide-group>
-        </v-sheet>
-
-        <!-- -----------------------------------------------
-            End Testimonial
-        ----------------------------------------------- -->
-      </v-container>
-    </div>
+    </v-container>
   </div>
 </template>
+
+
+<style scoped lang="scss">
+
+@keyframes first-loop {
+  0% { transform: translateX(0); }
+  100% { transform: translateX(-200%); }
+}
+
+@keyframes loop {
+  0% { transform: translateX(100%); }
+  100% { transform: translateX(-100%); }
+}
+
+.scroll-container {
+  --time: 2000s;
+  --half-time: 3s;
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+}
+
+.scroll-container .v-row {
+  display: flex;
+  animation: loop var(--time) linear infinite;
+  transform: translateX(100%);
+}
+
+.scroll-container .v-row > .v-col[data-first] {
+  animation: first-loop var(--time) linear forwards;
+  transform: translateX(0);
+}
+
+.scroll-container .v-row > .v-col[data-last] {
+  animation-delay: var(--half-time);
+}
+
+.scroll-container .v-card {
+  width: 330px;
+  height: 250px;
+  margin: 0 auto;
+}
+
+.fade {
+  pointer-events: none;
+  position: absolute;
+  inset: 0;
+  background: linear-gradient(
+          90deg,
+          var(--back-color),
+          transparent 30%,
+          transparent 70%,
+          var(--back-color)
+  );
+}
+</style>
