c3f9d3810d9bcdfee6efb96dc555d37341553149
AmirHossein <amirhidevs@gmail.com>
2024-07-23 01:02:20 +0330

    add lazy load to the map section

--- components/section/contact/Contact.vue
+++ components/section/contact/Contact.vue
@@ -100,7 +100,7 @@ const props = defineProps({
         :src="`https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3236.4952624612806!2d
         ${props.contactusSettings?.map.lng}!3d${props.contactusSettings?.map.lat}!
         2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzXCsDQ3JzE2LjAiTiA1McKwMjInNTkuMCJF!5e0!3m2!1sen!2s!4v172009
-        5973573!5m2!1sen!2s`" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade">
+        5973573!5m2!1sen!2s`" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"  loading="lazy">
     </iframe>
   </div>
 
